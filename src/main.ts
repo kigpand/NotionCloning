00b83d38dc5f3b9df9e0927977e58925e3fad649
kigpand <kigpand@gmail.com>
2021-08-02 21:56:25 +0900

    add closeBtn

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
+import { Component } from './components/component.js';
 import { TodoComponent } from './components/item/todo.js';
 import { NoteComponent } from './components/item/note.js';
 import { ImageComponent } from './components/item/image.js';
 import { VideoComponent } from './components/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
 
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-    image.attachTo(appRoot,'beforeend');
+    this.page.addChild(image);
 
     const video = new VideoComponent('Video Title', 'https://youtu.be/-KMZJor-FzY?list=RDMM');
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
